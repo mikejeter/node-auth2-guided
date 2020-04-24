7cad6642cb3b44745f611d8050f16cdc16ed743a
mikejeter <mjeter45@yahoo.com>
2020-04-24 19:40:34 -0400

    Created get token methos to auth router added json web token

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const authRouter = require('../auth/auth-router.js');
@@ -16,6 +17,25 @@ server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
+});
+
+server.get('/token', (req, res) => {
+
+  const payload = {
+    subject: 'thisuser',
+    userid: 'mjeter',
+    favoriteFood: 'papadia'
+  }
+
+  const secret = 'mysecret';
+
+  const options = {
+    expiresIn: '30m'
+  }
+
+  const token = jwt.sign(payload, secret, options);
+
+  res.json(token);
 });
 
 module.exports = server;
